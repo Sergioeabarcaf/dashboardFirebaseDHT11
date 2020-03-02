3bdf3a2c00d4f8ab00567fd9741f3b56892bc8da
sergioeabarcaf <sergioaf1991@gmail.com>
2020-03-02 18:02:53 -0300

    feat: Create bases dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphLineComponent } from './components/graph-line/graph-line.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphLineComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/graph-line/graph-line.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graph-line',
+  templateUrl: './graph-line.component.html',
+  styleUrls: ['./graph-line.component.css']
+})
+export class GraphLineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
