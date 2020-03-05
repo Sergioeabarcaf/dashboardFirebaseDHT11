9cd0aaee74fc0f34bbf4bc70930ffceabf21915c
sergioeabarcaf <sergioaf1991@gmail.com>
2020-03-05 16:44:36 -0300

    feat: connect data firebase service with table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'component-table',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _firebaseService: FirebaseService) {
+    
+  }
 
   ngOnInit(): void {
   }
