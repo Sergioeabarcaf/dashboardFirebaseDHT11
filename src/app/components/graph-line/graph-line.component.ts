06209fc0362aceb20c3b1176a297dab40dcf1232
sergioeabarcaf <sergioaf1991@gmail.com>
2020-03-05 10:50:00 -0300

    feat: Use ngx-echarts and create line chart

--- src/app/components/graph-line/graph-line.component.ts
+++ src/app/components/graph-line/graph-line.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EChartOption } from 'echarts';
 
 @Component({
   selector: 'graph-line',
@@ -6,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graph-line.component.css']
 })
 export class GraphLineComponent implements OnInit {
+
+  chartOption: EChartOption = {
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c}'
+    },
+    legend: {
+        left: 'left',
+        data: ['Temperatura', 'Humedad']
+    },
+    xAxis: {
+      type: 'category',
+      name: 'Hora',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: 'Temperatura',
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
+      },
+      {
+        name: 'Humedad',
+        data: [100, 102, 103, 150, 200, 400, 500],
+        type: 'line',
+        smooth: true
+      }
+    ]
+  }
 
   constructor() { }
 
